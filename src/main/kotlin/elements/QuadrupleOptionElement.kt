168a629c21b275aa8dc31a4859e323630d2481df
moritz <theincxption@gmail.com>
2021-01-17 20:23:08 +0100

    :bug: Fix Quadruple Element reading and writing

--- src/main/kotlin/elements/QuadrupleOptionElement.kt
+++ src/main/kotlin/elements/QuadrupleOptionElement.kt
@@ -2,20 +2,19 @@ package elements
 
 import interaction.takeMouse
 import interaction.takeScreen
-import ocr.debugFile
 import java.awt.Color
 
 const val QUADRUPLE_VALUE_WIDTH = 144 // the third one is 144, all others 143
 
 class QuadrupleOptionElement(name: String) : OptionElement<Int>(name) {
     override fun readValue(x: Int, y: Int): Int {
-        val valueX = x + width - (QUADRUPLE_VALUE_WIDTH * 4) + 1
+        val valueX = x + width - (QUADRUPLE_VALUE_WIDTH * 4) + 3
 
         val capture = takeScreen().capture(valueX, y, QUADRUPLE_VALUE_WIDTH * 4, height)
-        val color0 = Color(capture.getRGB(1, 1), true)
-        val color1 = Color(capture.getRGB(QUADRUPLE_VALUE_WIDTH + 1, 1), true)
-        val color2 = Color(capture.getRGB((QUADRUPLE_VALUE_WIDTH * 2) + 1, 1), true)
-        val color3 = Color(capture.getRGB((QUADRUPLE_VALUE_WIDTH * 3) + 1, 1), true)
+        val color0 = Color(capture.getRGB(1, 5), true)
+        val color1 = Color(capture.getRGB(QUADRUPLE_VALUE_WIDTH + 1, 5), true)
+        val color2 = Color(capture.getRGB((QUADRUPLE_VALUE_WIDTH * 2) + 1, 5), true)
+        val color3 = Color(capture.getRGB((QUADRUPLE_VALUE_WIDTH * 3) + 1, 5), true)
 
         val colors = listOf(color0, color1, color2, color3)
         val brightest = colors.find { color -> colors.all { color == it || color.isBrighter(it) } }
@@ -23,7 +22,7 @@ class QuadrupleOptionElement(name: String) : OptionElement<Int>(name) {
     }
 
     override fun writeValue(x: Int, y: Int, value: Int) {
-        val valueX = x + width - (QUADRUPLE_VALUE_WIDTH * 4) + 1
+        val valueX = x + width - (QUADRUPLE_VALUE_WIDTH * 4) + 3
         val targetX = valueX + (QUADRUPLE_VALUE_WIDTH * value)
 
         takeMouse().move(targetX + (QUADRUPLE_VALUE_WIDTH / 2), y + (height / 2)).click()
