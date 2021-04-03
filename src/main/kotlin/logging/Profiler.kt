b8b82cb03c2fcbe0e80035a3b25ddbc4399034d5
moritz <theincxption@gmail.com>
2021-04-03 18:03:46 +0200

    :art: Disable the profiler by default

--- src/main/kotlin/logging/Profiler.kt
+++ src/main/kotlin/logging/Profiler.kt
@@ -5,9 +5,10 @@ import java.util.*
 object Profiler {
     private var sectionLabel: Stack<String> = Stack()
     private var sectionStart: Stack<Long> = Stack()
+    private var enable = false
 
     fun section(label: String) {
-        Logger.debug("${Color.GREEN}▲ ${Color.WHITE_BRIGHT}$label")
+        if (enable) Logger.debug("${Color.GREEN}▲ ${Color.WHITE_BRIGHT}$label")
         sectionLabel.push(label)
         sectionStart.push(System.currentTimeMillis())
     }
@@ -16,7 +17,7 @@ object Profiler {
         if (!sectionLabel.empty()) {
             val label = sectionLabel.pop()
             val duration = System.currentTimeMillis() - sectionStart.pop()
-            Logger.debug("${Color.RED}▼ ${Color.WHITE_BRIGHT}$label ${Color.WHITE}(${Color.YELLOW}${duration}ms${Color.WHITE})")
+            if (enable) Logger.debug("${Color.RED}▼ ${Color.WHITE_BRIGHT}$label ${Color.WHITE}(${Color.YELLOW}${duration}ms${Color.WHITE})")
         }
     }
 }
