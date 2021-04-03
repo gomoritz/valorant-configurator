dd06fc60126b6f208fbf1338fee209c63b8c7211
moritz <theincxption@gmail.com>
2021-04-03 21:50:13 +0200

    :wrench: Update buildscript to version 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    id("org.beryx.runtime") version "1.12.1"
+    application
 }
 
 group = "net.theincxption"
-version = "1.0"
+version = "2.0"
 
 repositories {
     mavenCentral()
@@ -17,36 +17,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.3")
     implementation("com.fasterxml.jackson.core:jackson-core:2.11.3")
     implementation("org.jdom:jdom:2.0.0")
+    implementation("com.xenomachina:kotlin-argparser:2.0.7")
 }
 
 application {
-    mainClass.set("launcher.Launcher")
-}
-
-runtime {
-    modules.set(listOf("java.logging", "java.desktop", "java.datatransfer", "java.xml"))
-
-    jpackage {
-        imageName = "Valorant Configurator"
-//        imageDir.set(File("./build/Valorant Configurator"))
-//        imageOutputDir = File("./build/Valorant Configurator")
-
-        installerType = "msi"
-        installerOptions = listOf(
-            "--name", "\"Valorant Configurator\"",
-            "--win-shortcut",
-            "--win-dir-chooser",
-            "--win-menu",
-            "--win-menu-group", "\"Inception Cloud\"",
-            "--description", "\"Copy & paste settings for Valorant\"",
-            "--vendor", "\"Inception Cloud\"",
-            "--file-associations", "FAvalo.properties"
-        )
-        imageOptions = listOf(
-            "--name", "\"Valorant Configurator\"",
-            "--icon", "icon.ico"
-        )
-    }
+    mainClass.set("ValorantConfigurator")
 }
 
 tasks.withType<KotlinCompile>() {
