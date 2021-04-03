06e0dde0c25e359b3827c7c47b151ff6c2210877
moritz <theincxption@gmail.com>
2021-04-03 18:27:13 +0200

    :ambulance: Keep writing process alive though missing configuration entries to provide interoperability between multiple versions

--- src/main/kotlin/settings/SettingsWriter.kt
+++ src/main/kotlin/settings/SettingsWriter.kt
@@ -19,33 +19,37 @@ fun writeSettings(structure: StructureTree, file: File) = "Write settings" {
 
     SettingsTraverser(structure) { element, x, y ->
         try {
-            val target = (sectionObject ?: tabObject) ?: categoryObject!!
-            val value = target.get(element.name.toCamelCase())
+            val target = (sectionObject ?: tabObject) ?: categoryObject
+            val value = target?.get(element.name.toCamelCase())
 
-            when (element) {
-                is SliderOptionElement -> element.writeValue(x, y, value.doubleValue())
-                is DropdownOptionElement -> element.writeValue(x, y, value.intValue())
-                is SwitchOptionElement -> element.writeValue(x, y, value.booleanValue())
-                is TripleOptionElement -> element.writeValue(x, y, value.intValue())
-                is QuadrupleOptionElement -> element.writeValue(x, y, value.intValue())
-                is KeybindOptionElement -> element.writeValue(x, y, mapper.treeToValue<Keybind>(value)!!)
-                is SingleKeybindOptionElement -> element.writeValue(x, y, value.takeIf { it?.isNull == false }?.textValue())
-                is FieldOptionElement -> element.writeValue(x, y, value.doubleValue())
-                is UnlabelledSliderOptionElement -> element.writeValue(x, y, value.doubleValue())
-                is CheckboxElement -> element.writeValue(x, y, value.booleanValue())
+            if (value != null) {
+                when (element) {
+                    is SliderOptionElement -> element.writeValue(x, y, value.doubleValue())
+                    is DropdownOptionElement -> element.writeValue(x, y, value.intValue())
+                    is SwitchOptionElement -> element.writeValue(x, y, value.booleanValue())
+                    is TripleOptionElement -> element.writeValue(x, y, value.intValue())
+                    is QuadrupleOptionElement -> element.writeValue(x, y, value.intValue())
+                    is KeybindOptionElement -> element.writeValue(x, y, mapper.treeToValue<Keybind>(value)!!)
+                    is SingleKeybindOptionElement -> element.writeValue(x, y, value.takeIf { !it.isNull }?.textValue())
+                    is FieldOptionElement -> element.writeValue(x, y, value.doubleValue())
+                    is UnlabelledSliderOptionElement -> element.writeValue(x, y, value.doubleValue())
+                    is CheckboxElement -> element.writeValue(x, y, value.booleanValue())
+                }
+            } else {
+                Logger.info("Skipping setting <${element.name}>", "SettingsWriter")
             }
         } catch (e: Exception) {
             Logger.error("Failed to apply value to setting <${element.name}>:")
             Logger.error(e.stackTraceToString())
         }
     }.handleCategory { category ->
-        categoryObject = objectNode.get(category.name.toCamelCase()) as ObjectNode
+        categoryObject = objectNode.get(category.name.toCamelCase()) as? ObjectNode
         tabObject = null
         sectionObject = null
     }.handleTab { tab ->
-        tabObject = categoryObject!!.get(tab.name.toCamelCase()) as ObjectNode
+        tabObject = categoryObject?.get(tab.name.toCamelCase()) as? ObjectNode
         sectionObject = null
     }.handleSection { section ->
-        sectionObject = (tabObject ?: categoryObject!!).get(section.name.toCamelCase()) as ObjectNode
+        sectionObject = (tabObject ?: categoryObject)?.get(section.name.toCamelCase()) as? ObjectNode
     }.start()
 }
