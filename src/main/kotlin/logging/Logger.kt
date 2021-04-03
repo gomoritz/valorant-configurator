fcb2e33aba7891ba1ee1a080091ebb6eb7380e96
moritz <theincxption@gmail.com>
2021-04-03 19:45:18 +0200

    :sparkles: Write log output to a log file

--- src/main/kotlin/logging/Logger.kt
+++ src/main/kotlin/logging/Logger.kt
@@ -1,15 +1,18 @@
 package logging
 
+import java.io.File
 import java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE
 import java.text.SimpleDateFormat
 
 object Logger {
+    private val logFile = File("valorant_configurator.log")
     private val stackWalker = StackWalker.getInstance(RETAIN_CLASS_REFERENCE)
     private val template = "${Color.BLACK_BRIGHT}[${Color.WHITE}%s${Color.BLACK_BRIGHT}] " +
             "[%s${Color.BLACK_BRIGHT}] " +
             "${Color.PURPLE_BRIGHT}%s%s " +
             "${Color.BLACK_BRIGHT}- " +
             "${Color.WHITE_BRIGHT}%s"
+    private const val templateWithoutColors = "[%s] [%s] %s%s - %s"
 
     private fun log(level: Level, message: Any?, logger: String) {
         val marginSize = 5 - level.name.length
@@ -18,8 +21,10 @@ object Logger {
 
         val withHighlights = message.toString().replace(Regex("<(.*?)>"), "${Color.CYAN_BRIGHT}$1${Color.WHITE_BRIGHT}")
         val formatted = template.format(date, level.color.toString() + level.name, margin, logger, withHighlights)
+        val formattedWithoutColors = templateWithoutColors.format(date, level.name, margin, logger, message)
 
         println(formatted)
+        logFile.appendText(formattedWithoutColors + "\n")
     }
 
     fun debug(message: Any?, logger: String = stackWalker.callerClass.simpleName) = log(Level.DEBUG, message, logger)
